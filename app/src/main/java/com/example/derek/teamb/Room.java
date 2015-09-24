f55f04c81d8456ddb85a1f29e41ea5f6857d8762
nikolas <nikolas.kosse@gmail.com>
2015-09-23 23:01:20 -0500

    created Room class with private variables roomNumber, roomNode, roomFloor, building_ID, and roomType.

---
+++ app/src/main/java/com/example/derek/teamb/Room.java
@@ -0,0 +1,64 @@
+package com.example.derek.teamb;
+
+/**
+ * Class to hold the information from the room table in the database.
+ */
+public class Room {
+
+    public Room() {
+    }
+
+    public Room(int roomNumber, int roomNode, int roomFloor, int building_ID, String roomType) {
+        this.roomNumber = roomNumber;
+        this.roomNode = roomNode;
+        this.roomFloor = roomFloor;
+        this.building_ID = building_ID;
+        this.roomType = roomType;
+    }
+
+    private int roomNumber;
+    private int roomNode;
+    private int roomFloor;
+    private int building_ID;
+    private String roomType;
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public int getRoomNode() {
+        return roomNode;
+    }
+
+    public void setRoomNode(int roomNode) {
+        this.roomNode = roomNode;
+    }
+
+    public int getRoomFloor() {
+        return roomFloor;
+    }
+
+    public void setRoomFloor(int roomFloor) {
+        this.roomFloor = roomFloor;
+    }
+
+    public int getBuilding_ID() {
+        return building_ID;
+    }
+
+    public void setBuilding_ID(int building_ID) {
+        this.building_ID = building_ID;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+}
